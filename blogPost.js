ac3903efffb9a664edec8eb8941b7e127aae8c72
Mahua2737 <141414997+Mahua2737@users.noreply.github.com>
2023-10-11 12:58:47 +0530

    Add files via upload

---
+++ blogPost.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+fs.writeFile('sample.txt', "Hello ", (err) => console.log('content updated'));
+// fs.appendFile('sample.txt', "Ancyyyy", (err) => console.log('content updated'));
+// fs.readFile('sample.txt', "utf-8", (err, data) => console.log(data));
+const path = require('path');
+
+fs.writeFile(path.join(__dirname, 'posts', 'blogPost.txt'), 'Hello', (err) => {
+    if (err) {
+        throw err;
+
+    }
+    console.log('file created');
+});
